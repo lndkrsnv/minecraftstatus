9666851210a39e40a477a62d5854e1f4908e9ee6
lndkrsnv <lndkrsnv@mail.ru>
2025-12-26 18:51:08 +0300

    CORS headers

--- src/main/java/com/lndkrsnv/minecraftstatus/controller/ServerStatusController.java
+++ src/main/java/com/lndkrsnv/minecraftstatus/controller/ServerStatusController.java
@@ -3,11 +3,9 @@ package com.lndkrsnv.minecraftstatus.controller;
 import com.lndkrsnv.minecraftstatus.service.MinecraftQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class ServerStatusController {
 
